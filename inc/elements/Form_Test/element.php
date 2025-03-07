8f0ef05c461658e044c4a27941cfd81ea1eaec6a
Nicolas Scott <nic@crearewebsolutions.com>
2025-03-06 19:17:05 -0500

    fix deprecation

--- inc/elements/Form_Test/element.php
+++ inc/elements/Form_Test/element.php
@@ -55,7 +55,7 @@ class FormTest extends \Breakdance\Elements\Element
 
     static function slug()
     {
-        return get_class();
+        return __CLASS__;
     }
 
     static function template()
